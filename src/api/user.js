f947121778c8e39b38b7eca6584cdac32dedb78c
Py-lambda-at-289631370416 <VIskFiUWw7LLZ7SqXe0MP+z4Zfp8uIFr70wczC/pfyc=>
2021-07-08 22:17:47 +0800

    user

--- src/api/user.js
+++ src/api/user.js
@@ -11,7 +11,7 @@ export function login(data) {
 
 export function getInfo(token) {
   return request({
-    url: '/vue-element-admin/user/info',
+    url: '/api/v1/user/info',
     method: 'get',
     params: { token }
   })
